25e1d218785a089279aa0efbe5cb45fa225fd28f
ChanLiitaa <elixtroannylalita@gmail.com>
2026-06-02 14:56:28 +0700

    Link Page to by module

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,18 +1,51 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 
 import MainLayout from "../layouts/MainLayout";
-import Dashboard from "../pages/Dashboard"; 
+import Dashboard from "../pages/Dashboard";
+import Users from "../pages/users/Users";
+import Customers from "../pages/customers/Customers";
+import Policies from "../pages/policies/Policies";
+import Payments from "../pages/payments/Payments";
+import Claims from "../pages/claims/Claims";
+import Reports from "../pages/reports/Reports";
 
 export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <MainLayout />,
-        children: [
-        {
-            path: "dashboard",
-            element: <Dashboard />,
-        },
-        
-        ],
-    },
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Navigate to="/dashboard" replace />,
+      },
+      {
+        path: "dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "users",
+        element: <Users />,
+      },
+      {
+        path: "customers",
+        element: < Customers />,
+      },
+      {
+        path: "policies",
+        element: < Policies />,
+      },
+      {
+        path: "payments",
+        element: < Payments />,
+      },
+      {
+        path: "claims",
+        element: < Claims />,
+      },
+       {
+        path: "reports",
+        element: < Reports />,
+      },
+    ],
+  },
 ]);
